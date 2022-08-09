6b398db5566c01566e8bbc082ab233c2b02bce12
Ryan J. Parker <ryan.j.parker@wsu.edu>
2022-08-09 10:37:27 -0700

    add fetch functions

--- detail/detail.js
+++ detail/detail.js
@@ -6,3 +6,5 @@
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+import { getACountry } from '../fetch-utils.js';
+import { renderCountryDetail } from '../render-utils.js';
